d1e3ffb56d0c552a29ed0a3e926024d64de9247e
Oleksii Dovhaniuk <alexdovhaniuk@gmail.com>
2023-12-10 01:24:22 +0200

    2023-12-10 finish on time start chart

--- src/scripts/01_setup.R
+++ src/scripts/01_setup.R
@@ -10,6 +10,8 @@
 # Import Packages ----------------------------------------------------------------
 
 library(tidyverse)
+library(lubridate)
+library(ggplot2)
 library(glue)
 
 
@@ -25,10 +27,10 @@ mng_theatres_df <- read.csv('src/data/raw/2023-11-26_tmm_1month_1hospital_mng_th
 
 # Split Manage Theatres Data Frame ----------------------------------------
 
-string_to_unix_timestamps <- function(
+to_unix_timestamp <- function(
     string_time, 
     tz = 'UTC',
-    format = '%H:%M:%OS',
+    format = '%H:%M',
     origin = '1970-01-01',
     optional = TRUE) {
   
@@ -55,21 +57,21 @@ theatre_core_time_df <- mng_theatres_df[6:12, 8:10] |>
     )) |>
   
   mutate(
-    theatre_open = string_to_unix_timestamps(theatre_open),
-    theatre_close = string_to_unix_timestamps(theatre_close),
+    theatre_open = to_unix_timestamp(theatre_open),
+    theatre_close = to_unix_timestamp(theatre_close),
     
     core_time_m = as.integer( 
       difftime(theatre_close, theatre_open, units = 'mins')
     ),
     core_time_h = as.numeric( 
       difftime(theatre_close, theatre_open, units = 'hours')
-    ),
-    
-    theatre_open = format(theatre_open, format = '%H:%M'),
-    theatre_close = format(theatre_close, format = '%H:%M')
+    )
+
+    # theatre_open = format(theatre_open, format = '%H:%M'),
+    # theatre_close = format(theatre_close, format = '%H:%M')
   )
 
-# View(theatre_core_time_df)
+View(theatre_core_time_df)
 
 
 # Note: it may be necessary to rewrite the following script
@@ -93,12 +95,12 @@ theatre_adhocs_df <- mng_theatres_df[6:8, c(13:14, 17:19)] |>
     
     open = ifelse(
       str_detect(open, '^\\s*$'), 
-      string_to_unix_timestamps('00:00:00'),
-      string_to_unix_timestamps(open) ),
+      to_unix_timestamp('00:00:00'),
+      to_unix_timestamp(open) ),
     close = ifelse(
       str_detect(close, '^\\s*$'), 
-      string_to_unix_timestamps('00:00:00'),
-      string_to_unix_timestamps(close) ),
+      to_unix_timestamp('00:00:00'),
+      to_unix_timestamp(close) ),
     
     adhoc_time_m = as.integer( 
       difftime(close, open, units = 'mins') ),
@@ -141,11 +143,11 @@ clean_data_df <- entry_data_df |>
     specialty = factor(specialty),
     surgery_status = factor(surgery_status),
     surgery_start_date = as.Date(surgery_start_date, '%d.%m.%Y'),
-    anaesthetic_start = string_to_unix_timestamps(anaesthetic_start),
-    surgery_start = string_to_unix_timestamps(surgery_start),
-    surgery_finish = string_to_unix_timestamps(surgery_finish),
-    anaesthetic_finish = string_to_unix_timestamps(anaesthetic_finish),
-    left_theatre = string_to_unix_timestamps(left_theatre),
+    anaesthetic_start = to_unix_timestamp(anaesthetic_start),
+    surgery_start = to_unix_timestamp(surgery_start),
+    surgery_finish = to_unix_timestamp(surgery_finish),
+    anaesthetic_finish = to_unix_timestamp(anaesthetic_finish),
+    left_theatre = to_unix_timestamp(left_theatre),
     surgery_end_date = as.Date(surgery_end_date, '%d.%m.%Y')) |> 
   select(
     -surgery_start,
@@ -165,10 +167,57 @@ View(clean_data_df)
 prepared_df <- clean_data_df |> 
   mutate(
     weekday = format(surgery_start_date, '%a'),
-    duration = ifelse(
-      difftime(anaesthetic_finish, anaesthetic_start, units = 'mins') > 0,
-      difftime(anaesthetic_finish, anaesthetic_start, units = 'mins'),
-      difftime(anaesthetic_start, anaesthetic_finish, units = 'mins')) )
+    anaesthetic_finish = case_when(
+      anaesthetic_finish >= anaesthetic_start ~ anaesthetic_finish,
+      TRUE ~ anaesthetic_finish + dhours(24)),
+    duration = difftime(
+      anaesthetic_finish, 
+      anaesthetic_start, 
+      units = 'mins'),
+    week = cut(
+      surgery_start_date, 
+      breaks = "week", 
+      labels = FALSE)) |> 
+  merge(theatre_core_time_df, by.y = "weekday") |> 
+  mutate(
+    early_start = case_when(
+      anaesthetic_start >= theatre_open ~ NA,
+      TRUE ~ difftime(theatre_open, anaesthetic_start, units = 'mins')
+    ),
+    over_run = case_when(
+      anaesthetic_start > theatre_close ~ NA,
+      theatre_close >= anaesthetic_finish ~ NA,
+      TRUE ~ difftime(anaesthetic_finish, theatre_close, units = 'mins')
+    ),
+    non_core = case_when(
+      anaesthetic_finish <= theatre_open ~ duration,
+      anaesthetic_start >= theatre_close ~ duration,
+      TRUE ~ NA
+    ),
+    core_time = case_when(
+      ( anaesthetic_start < theatre_open & 
+        anaesthetic_finish > theatre_open &
+        anaesthetic_finish <= theatre_close ) ~ 
+          difftime(anaesthetic_finish, 
+                   theatre_open, 
+                   units = 'mins'),
+      ( anaesthetic_start >= theatre_open & 
+        anaesthetic_finish <= theatre_close ) ~ 
+          duration,
+      ( anaesthetic_start >= theatre_open &
+        anaesthetic_start < theatre_close &
+        anaesthetic_finish > theatre_close ) ~ 
+          difftime(theatre_close, 
+                   anaesthetic_start, 
+                   units = 'mins'),
+      ( anaesthetic_start <= theatre_open & 
+        anaesthetic_finish >= theatre_close ) ~ 
+          difftime(theatre_close, 
+                   theatre_open, 
+                   units = 'mins'),
+      TRUE ~ NA
+    )
+  )
 View(prepared_df)
 
 
@@ -261,6 +310,129 @@ summary_df <- clean_data_df |>
 
 summary_df
 View(summary_df)
+
+
+
+# On Time Start Chart -----------------------------------------------------
+
+on_time_start_df <- prepared_df |> 
+  filter( !is.na(anaesthetic_start) ) |> 
+  group_by(surgery_start_date, theatre) |> 
+  
+  reframe(
+    week = week[which.min(anaesthetic_start)],
+    first_surgery_start = min(anaesthetic_start, na.rm = TRUE),
+    theatre_open = theatre_open[which.min(anaesthetic_start)],
+    duration = duration[which.min(anaesthetic_start)],
+    early_start = sum(early_start, rm.na = TRUE),
+    
+    late_start = case_when(
+      first_surgery_start + duration > theatre_open ~
+        difftime(first_surgery_start, theatre_open, units = 'mins'),
+      TRUE ~ NA),
+    
+    on_time = late_start <= 0 ) |> 
+  
+  # Grouping by weeks and theatres:
+  group_by(week, theatre) |>
+  reframe(
+    minutes_lost = sum(late_start[ which(late_start > 0) ]),
+    early_start = sum(early_start, rm.na = TRUE),
+    days_on_time = sum(on_time) / 5
+  ) |> 
+  filter(theatre == 'I')
+  
+
+# View(on_time_start_df)
+on_time_start_df
+
+
+# Create a plot
+on_time_start_p <- ggplot( 
+    on_time_start_df, 
+    aes(x = factor(week)) ) +
+  
+  geom_bar(
+    aes(y = minutes_lost, fill = "Mins lost"), 
+    stat = "identity", 
+    # fill = "orange", 
+    alpha = 0.7,
+    position = "identity") +
+  
+  geom_line(
+    aes(y = days_on_time * 600, color = "% Days on-time start"), 
+    # color = "green", 
+    group = 1) +
+  
+  geom_point(
+    aes(y = days_on_time * 600, color = "% Days on-time start"), 
+    shape = 15, 
+    # color = "green",
+    size = 3,
+    alpha = 0.7) +  # Add square points
+  
+  geom_label(
+    aes(y = days_on_time * 600, 
+        label = scales::percent(days_on_time, scale = 100)), 
+    vjust = -1,
+    hjust = 0.4,
+    color = "black",
+    label.padding = unit(0.2, "lines")) +  # Add labels on top of the line
+  
+  scale_y_continuous(
+    sec.axis = sec_axis(
+      ~./600, 
+      name = "Days On-Time Start (%)",
+      breaks = seq(0, 1, by = 0.2),
+      labels = scales::percent_format(scale = 100)),
+    
+    name = "Minutes Lost due to Late Start",
+    breaks = seq(0, 600, by = 100),
+    limits = c(0, 600)
+  ) +
+  
+  scale_x_discrete(
+    labels = paste0("Wk ", on_time_start_df$week)) +
+  
+  scale_fill_manual( 
+    name = '', 
+    values = c("Mins lost" = "#faab36",
+               "Mins early" = '#fd5901') ) +
+  
+  scale_color_manual( 
+    name = '', 
+    values = c("% Days on-time start" = "#249ea0") ) +
+  
+  labs(
+    title = "On Time Start (AM)", 
+    x = "Weeks", 
+    y = "Minutes Lost due to Late Start",
+    fill = "Time" ) +
+  
+  theme_minimal() +
+  theme(
+    panel.grid.major.x = element_blank(),  # Remove y-axis grid lines
+    legend.position = "bottom",
+    legend.direction = "horizontal" )
+
+on_time_start_p
+
+# Inter Operational Interval Chart ----------------------------------------
+
+
+# Early Finish/ Over Runs Chart -------------------------------------------
+
+
+# % Utilisation and Number of Cases in Core Hours -------------------------
+
+
+# % Utilisation and Number of Cases in Non-Core Hours ---------------------
+
+
+# % Utilisation Pure Core with Non-Core Time  ---------------------------------------------
+
+
+
 
 
 
